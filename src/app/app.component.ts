cbe4d2abfe6dfa3bcb260ddb0d26de3335fcbb7b
Lucas Peixoto <lspeixotodev@gmail.com>
2024-09-23 10:02:20 -0300

    feat: config material, tailwindcss and integrate material dark/light theme with tailwind theme config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'life-manage-ui';
+  
 }
